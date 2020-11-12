c504c878ca86a56a9c46d72569902478f3b98087
JOSÉ DAVID <jose51949@gmail.com>
2020-11-12 09:57:20 -0600

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -15,3 +15,34 @@ print(30.3)
 print(False)
 print(True)
 
+# List - Arrays
+# Los datos se pueden cambiar
+[10, 20, 30]
+Hello = ['Hello', 'World']
+print(Hello)
+
+# Tuples
+# Las Tuples no pueden cambiarse
+(10, 20, 30, 55)
+
+# Dictorionies
+myInfo = {
+    "name": "Jose David",
+    "lastname": "Jimenez Badilla",
+    "nickname": "Nazzer"
+}
+
+print(myInfo)
+
+# Imprime el tipo de dato
+print(type({
+    "color": "red",
+    "type": "Toyota"
+}))
+#{ "key": "value" }
+
+# Tipo de dato no definido - Default
+None
+print(None)
+
+
