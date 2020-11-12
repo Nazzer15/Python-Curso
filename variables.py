9328545ee0af7efcd2e189a0de7aee7d554f0bea
JOSÉ DAVID <jose51949@gmail.com>
2020-11-12 10:13:39 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Definicion de variables
+name = "Jose David"
+
+# Case sensitive
+book = "Digital Fortress"
+Book = "Clean Code"
+
+print(book + " " + Book);
+
+x, y = 100, 300
+print(x, y)
+# La coma , añade un espacio entre las variables
