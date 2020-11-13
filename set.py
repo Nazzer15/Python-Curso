6a58e0397d718b4de261cc3601d24291fd5aab48
JOSÉ DAVID <jose51949@gmail.com>
2020-11-13 11:06:25 -0600

    Create set.py

---
+++ set.py
@@ -0,0 +1,18 @@
+colors = {'Red', 'Green', 'Blue'}
+
+print(colors)
+# Buscar un color
+print('Red' in colors)
+
+# Añade un color al Set
+colors.add('Purple')
+print(colors)
+# Elimina 1
+colors.remove('Red')
+print(colors)
+#Limpia el set
+colors.clear()
+print(colors)
+
+
+
