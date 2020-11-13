eccb9f7195c790e8faa170ccccf3ed23213a998d
JOSÉ DAVID <jose51949@gmail.com>
2020-11-13 10:53:50 -0600

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,31 @@
+# Es mas comun este metodo
+x = (1, 2, 3)
+#print(x)
+# crea una Tuple
+y = tuple((4,5,6))
+
+#print(y)
+#print(dir(y))
+
+# crear una tuple con un solo elemento 
+uno = (41,)
+#print(type(uno))
+#print(uno)
+
+#Mostrar un solo elemento
+x = (1, 2, 3, 4)
+print(x[3])
+# La tuplas NO se editan
+
+
+locations = {
+    (35.1345, 95.0000): "Tokio",
+    (65.00, 98.23): "New York"
+}
+
+
+
+
+
+
+  
