623d974954542cc384d673fa7573271a83ed1ac4
JOSÉ DAVID <jose51949@gmail.com>
2020-11-16 09:27:42 -0600

    Creacion de dictionaries
    Varios metodos sobre dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,36 @@
+shopping_car = {
+    "name": "books",
+    "quantity": 3,
+    "price": 14.99
+}
+
+person = {
+    "first_name" : "Jose David",
+    "last_name": "Jimenez Badilla"
+}
+
+#print(shopping_car)
+# Obtener los valores de las llaves
+print(person.keys())
+# Obtiene los resultados o los values
+print(person.items())
+
+# Limpia el diccionario
+person.clear()
+print(person)
+
+# Borra person
+#del person
+#print(person)
+
+product = [
+    {"name" : "book", "price": "14.99"},
+    {"name" : "laptop", "price": "899.95"}
+]
+
+print(product)
+
+
+
+
+
