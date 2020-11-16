ee35644bc63c9c9f118914406201bf1e37aaf604
JOSÉ DAVID <jose51949@gmail.com>
2020-11-16 11:46:03 -0600

    Modulos, fmath libreria custom

---
+++ modules.py
@@ -0,0 +1,27 @@
+# modulos o bibliotecas
+#own modules
+# thirdy party modules
+# python modules
+
+# Trae toda la libreria 
+import datetime
+
+
+
+#from datetime import timedelta, date
+
+print(datetime.date.today())
+
+print(datetime.timedelta(minutes= 180))
+
+# Import custom modules
+
+import fmath
+#from fmath import add, substract
+#add(1,2)
+#substract(12, 65)
+
+fmath.add(10, 5)
+
+fmath.substract(15, 35)
+
